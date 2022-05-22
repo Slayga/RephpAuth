d5b707864b46ad996c3ff2e63a665c18daf84208
Gabriel Engberg <gabriel.engberg123@gmail.com>
2022-05-22 19:43:01 +0200

    first styling

--- re-re/index.php
+++ re-re/index.php
@@ -50,6 +50,10 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Include main stylesheet -->
+    <link rel="stylesheet" href="styles/main.css">
+
     <title>Document</title>
 </head>
 
