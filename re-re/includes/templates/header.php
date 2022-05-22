c39cef7d262b623392e499dd62cd730a5643aba6
Gabriel Engberg <gabriel.engberg123@gmail.com>
2022-05-22 20:52:52 +0200

    generic navbar

---
+++ re-re/includes/templates/header.php
@@ -0,0 +1,18 @@
+<h1>Re-Re Gallery</h1>
+<nav>
+    <a href="#">Home</a>
+    <a href="#">About</a>
+    <a href="#">Contact</a>
+</nav>
+<div class="header__form-wrap">
+    <?php if ($auth->is_logged) {include_once __DIR__ . "/../profile.inc.php";} ?>
+    <?php if($auth->is_logged) {include_once __DIR__ . "/../logout.inc.php";} ?>
+</div>
+
+<style>
+.header__form-wrap {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
